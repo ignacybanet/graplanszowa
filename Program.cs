6a6f0ff092ef15f34775febe59f0bea53d03a700
ignacy <adsa@asda.com>
2024-12-10 16:02:46 +0100

    implement player constructor, movePlayer method, special fields functionality

--- Program.cs
+++ Program.cs
@@ -8,25 +8,37 @@ class Program {
     
     class Player {
         public string Name;
-        public string Position;
+        public int Position;
         public int Score;
-         
-        static void MovePlayer(string playerName) {
-            // kod
+        public int Id;
+
+        public Player(string playerName, int playerId) {
+            Name = playerName;
+            Id = playerId;
+            Position = 0;
+            Score = 0;
         }
+
+         
+        
 
         public int RollDice() { // zrobione
             Random rnd = new Random();
             int RolledValue = rnd.Next(1,6);
             return RolledValue;
         }
+
+        public void MovePlayer(string playerName) { // jest juz git prosze nie zmieniac
+            int IncrementPosition = RollDice();
+            Position += IncrementPosition;
+            Console.WriteLine($"Player {playerName} just moved by {IncrementPosition} fields.");
+        }
     }
 
     class Board {
-        // konstruktor dla planszy
-
+        
         public int BoardSize;
-        public int[] BoardSpecial;
+        public List<int> BoardSpecial;
 
         public Board(int size = 100) {
             
@@ -37,16 +49,18 @@ class Program {
                 BoardSize = size; 
             }
 
+            BoardSpecial = new List<int>();
+            
+
         }
 
         public void GenerateBoard() {
         
-            int specialAmount = BoardSize / 6;
-            BoardSpecial = new int[specialAmount + 1];
+            int specialFieldsAmount = BoardSize / 6;
             Random rnd = new Random();
 
-            for(int i = 0 ; i != BoardSpecial.Length ; i++) {
-                BoardSpecial[i] = rnd.Next(BoardSize);
+            for(int i = 0 ; i != specialFieldsAmount ; i++) {
+                BoardSpecial.Add( rnd.Next(BoardSize) );
             }
             
         }
@@ -58,8 +72,11 @@ class Program {
             // kod
         }
 
-        static void CheckForSpecialPlace() {
-            // sprawdzenie czy w danym miejscu jest specjalne pole
+        public static void CheckForSpecialField(Player player, Board board) { // Funkcja przyjmuje gracza (typ danych Player) oraz plansze (typ danych Board)
+            if( board.BoardSpecial.Contains( player.Position ) ) { // sprawdzenie czy w danym miejscu jest specjalne pole
+                player.Score++;
+                Console.WriteLine($"{player.Name} just entered a special field! \nTheir score has been incremented by 1!");
+            }
         }
 
         public void ShowResults() {
@@ -73,6 +90,8 @@ class Program {
 
     static void Main(string[] args) {
         // kod
+
+        Player playerOne = new Player("Lukasz", 0);
     }
 
 }
